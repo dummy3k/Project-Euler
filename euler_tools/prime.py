9caab2bcc9b24f43475fec5de53fd16aeb2a150e
dummy3k <l4711@gmx.net>
2010-03-07 21:36:49 +0100

    progress bar

--- euler_tools/prime.py
+++ euler_tools/prime.py
@@ -1,10 +1,18 @@
 import math
 
-def is_prime(number):
-    for divisor in range(2, int(math.sqrt(number)) + 1):
+def is_prime(number, primes=None):
+    if not primes:
+        start_at = 2
+    else:
+        for m in primes:
+            if number % m == 0:
+                return False
+
+        start_at = m + 1
+
+
+    for divisor in range(start_at, int(math.sqrt(number)) + 1):
         if number % divisor == 0:
             return False
 
     return True
-
-
---
+++ problem010/solution.py
@@ -0,0 +1,72 @@
+"""
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+"""
+if __name__ == "__main__":
+    import sys, os
+    sys.path.append( os.path.join( os.getcwd(), '..' ) )
+
+import sys
+from datetime import datetime
+from euler_tools.prime import is_prime
+
+class ProgressBar():
+    def __init__(self, max):
+        self.max = max
+        self.value = 0
+        self.last_update = None
+        self.tty_width = 40
+
+    def set_value(self, value):
+        if value == self.value:
+            return
+
+        self.value == value
+        if not self.last_update or (datetime.now() - self.last_update).seconds > 1:
+            percent = value * 1. / self.max
+            msg = "\r["
+            for n in range(self.tty_width - 2):
+                if float(n) / self.tty_width > percent:
+                    msg += "_"
+                else:
+                    msg += "*"
+
+            #~ print "\r%s" % percent,
+            msg += "]"
+            print msg,
+            sys.stdout.flush()
+            self.last_update = datetime.now()
+
+    def finished(self):
+        msg = "\rFinished"
+        while len(msg) - 1 < self.tty_width:
+            msg += " "
+
+        print msg
+
+start_time = datetime.now()
+sum = 0
+primes = []
+progress = ProgressBar(2000000)
+
+#~ for n in range(2, 2000000):
+for n in range(2, progress.max):
+    progress.set_value(n)
+    break_out = False
+
+    if break_out:
+        continue
+
+    #~ if is_prime(n, primes):
+    if is_prime(n):
+        sum += n
+        primes.append(n)
+
+progress.finished()
+print "sum: %s" % sum
+end_time = datetime.now()
+#~ print primes
+print end_time - start_time
+
+#2,000,000: 142913828922
