f7f5e66e8b1048ab5308e27d465b3e6a2303b014
dummy3k <l4711@gmx.net>
2010-03-08 22:16:33 +0100

    problem045 solved

--- euler_tools/misc.py
+++ euler_tools/misc.py
@@ -6,4 +6,23 @@ def is_palindrome(x):
 
     return True
 
+do_debug = True
+def ipython():
+    """ break and jump into ipython. call it this way:
+            h.debug()('foo')
+    """
+
+    if not do_debug:
+        def noop():
+            pass
+        return noop
+
+    from IPython.Shell import IPShellEmbed
+    # '-pdb',
+    args = ['-pi1', 'In <\\#>: ', '-pi2', '   .\\D.: ',
+            '-po', 'Out<\\#>: ', '-nosep']
+    ipshell = IPShellEmbed(args,
+        banner = 'Entering IPython.  Press Ctrl-D to exit. Set h.do_debug=False to never go here again.',
+        exit_msg = 'Leaving Interpreter, back to Pylons.')
+    return ipshell
 
